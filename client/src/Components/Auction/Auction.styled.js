7b1cdc89d4823880be8ce5ce94bf180b72d5553a
Chornyi Vlad <4ornii.vlad@gmail.com>
2022-04-22 19:35:13 +0300

    add styles

---
+++ client/src/Components/Auction/Auction.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  padding-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .ant-table-wrapper {
+    margin-bottom: 40px;
+  }
+`;
---
+++ client/src/Components/Header/Header.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+  background-color: #b3dcfd;
+  height: 50px;
+
+  .title {
+    font-size: 25px;
+    color: white;
+  }
+`;
